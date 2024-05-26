52a8415826285cbce4f9c37ea209eb85f2dadaf7
Henk Boschma <henkboschma@hotmail.com>
2024-05-26 12:04:29 +0200

    Test

--- src/main/java/guru/springframework/Greating.java
+++ src/main/java/guru/springframework/Greating.java
@@ -8,6 +8,7 @@ public class Greating {
     private static final String HELLO = "Hello";
     private static final String WORLD = "World";
 
+    String test= "test";
     public String helloWorld(){
         return HELLO + " " + WORLD;
     }
